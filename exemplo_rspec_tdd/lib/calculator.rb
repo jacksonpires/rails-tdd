6e9a626292f55cd33b64b9caa78fc97a28e6efa2
Jackson Pires <jackson.pires@gmail.com>
2017-08-16 22:27:12 +0000

    Matchers de Erros

--- exemplo_rspec_tdd/lib/calculator.rb
+++ exemplo_rspec_tdd/lib/calculator.rb
@@ -2,4 +2,8 @@ class Calculator
   def sum(a, b)
     a + b
   end
+
+  def div(a, b)
+    a / b
+  end
 end
--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -2,6 +2,15 @@ require 'calculator'
 
 describe Calculator, "Sobre a Calculadora" do
 
+  context '#div' do
+    it 'divide by 0' do
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3, 0)}.to raise_error("divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, "divided by 0")
+      expect{subject.div(3, 0)}.to raise_error(/divided/)
+    end
+  end
+
   context '#sum' do
     it 'with positive numbers' do
       result = subject.sum(5, 7)
