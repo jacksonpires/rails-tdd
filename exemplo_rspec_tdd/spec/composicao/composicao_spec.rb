663804fb35aadf19ce4c4ff2553d5024c9910532
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-08-21 19:34:21 -0300

    Composição de Expectativas

---
+++ exemplo_rspec_tdd/spec/composicao/composicao_spec.rb
@@ -0,0 +1,8 @@
+describe 'Ruby on Rails' do
+  it { is_expected.to start_with('Ruby').and end_with('Rails') }
+  it { expect(fruta).to eq('banana').or eq('laranja').or eq('uva')  }
+
+  def fruta
+    %w(banana laranja uva).sample
+  end
+end
