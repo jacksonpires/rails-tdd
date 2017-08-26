fc07187e6b19edce4b9ee4a4972abe02ea40476b
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-08-26 14:52:13 -0300

    Negativando Matchers

--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,8 +1,12 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1,2,3]), "Array" do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(2,1)
   end
+
+  it { expect(subject).to exclude(4) }
 
   it '#contain_exactly' do
     expect(subject).to contain_exactly(2,1,3)
--- exemplo_rspec_tdd/spec/matchers/be_within/be_within_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/be_within/be_within_spec.rb
@@ -1,5 +1,8 @@
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
+
 describe 'be_within' do
   it { expect(11.5).to be_within(0.5).of(12.0) }
+  it { expect(10.0).to be_not_within(0.5).of(12.0) }
   it { expect([11.6, 12.1, 12.4]).to all (be_within(0.5).of(12.0))}
 end
 
