9396f91d51da50d8f89d5307f75e6e7d892de792
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-10-04 14:33:52 -0300

    Tag Filters

--- exemplo_rspec_tdd/spec/matchers/all/all_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/all/all_spec.rb
@@ -1,4 +1,4 @@
-describe 'all' do
+describe 'all', type: 'collection' do
   it { expect([1,7,9]).to all( (be_odd).and be_an(Integer) )}
   it { expect(['ruby', 'rails']).to all( be_a(String).and include('r') )}
 end
--- exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/arrays/arrays_spec.rb
@@ -1,6 +1,6 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]), "Array" do
+describe Array.new([1,2,3]), "Array", type: 'collection' do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(2,1)
@@ -8,7 +8,7 @@ describe Array.new([1,2,3]), "Array" do
 
   it { expect(subject).to exclude(4) }
 
-  it '#contain_exactly' do
+  it '#contain_exactly', :slow do
     expect(subject).to contain_exactly(2,1,3)
   end
 
