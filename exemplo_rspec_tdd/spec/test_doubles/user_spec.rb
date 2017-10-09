44aa584cc14dc6a62bf7ad38b80c5ede8af038c6
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-10-09 19:52:55 -0300

    Método As Null Object

--- exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
+++ exemplo_rspec_tdd/spec/test_doubles/user_spec.rb
@@ -6,4 +6,13 @@ describe 'Test Double' do
     puts user.name
     puts user.password
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+    allow(user).to receive(:name).and_return('Jack')
+    allow(user).to receive(:password).and_return('secret')
+    puts user.name
+    puts user.password
+    user.abc
+  end
 end
