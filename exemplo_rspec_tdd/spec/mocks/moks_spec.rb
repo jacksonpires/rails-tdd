9f8f2ec328ca53ae4e993e2690c3f0641ef6429e
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-10-09 19:35:32 -0300

    Mock Expectations

--- exemplo_rspec_tdd/spec/mocks/moks_spec.rb
+++ exemplo_rspec_tdd/spec/mocks/moks_spec.rb
@@ -12,4 +12,23 @@ describe 'Mocks' do
     # exercise
     student.bar
   end
+
+  it 'args' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123)
+    student.foo(123)
+  end
+
+  it 'repetição' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).twice
+    student.foo(123)
+    student.foo(123)
+  end
+
+  it 'retorno' do
+    student = Student.new
+    expect(student).to receive(:foo).with(123).and_return(true)
+    puts student.foo(true)
+  end
 end
