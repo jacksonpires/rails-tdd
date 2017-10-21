06bf3ff5e4962c920414f5251a382d375c97c6b5
Jackson Pires <jackson.pires@gmail.com>
2017-10-21 18:08:09 +0000

    Começando com Fixtures

--- test_app/app/models/customer.rb
+++ test_app/app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+    def full_name
+      "Sr. #{name}"
+    end
 end
--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  fixtures :all
+
+  it 'Create a Customer' do
+    customer = customers(:jackson)
+    expect(customer.full_name).to eq("Sr. Jackson Pires")
+  end
 end
