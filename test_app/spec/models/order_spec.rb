a9a502b720c9f9f286b57b6062ba0c72ac50edbd
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-11-12 12:10:25 -0300

    Create List

--- test_app/spec/models/order_spec.rb
+++ test_app/spec/models/order_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Order, type: :model do
     order = create(:order)
     expect(order.customer).to be_kind_of(Customer)
   end
+
+  it 'Tem 3 pedidos' do
+    orders = create_list(:order, 3)
+    expect(orders.count).to eq(3)
+  end
 end
