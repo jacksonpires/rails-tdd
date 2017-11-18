ecc3db8f59ce4f9e59a8140d7d3f534d1e6b8b12
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-11-18 18:22:38 -0300

    Webmock

--- test_app/Gemfile
+++ test_app/Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
   gem 'factory_bot_rails', '~> 4.0'
   gem 'faker'
   gem 'httparty'
+  gem 'webmock'
 end
 
 group :development do
--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,5 +1,8 @@
 describe 'HTTParty' do
-  it 'HTTParty' do
+  it 'content-type' do
+    stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+         to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+
     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
