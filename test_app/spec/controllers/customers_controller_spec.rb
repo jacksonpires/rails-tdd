68f381c290fe51c932f7caa123812ce613af44a7
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-12-07 21:22:53 -0300

    Content-Type

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -28,6 +28,13 @@ RSpec.describe CustomersController, type: :controller do
       @customer = create(:customer)
     end
 
+    it 'Content-Type JSON' do
+      customer_params = attributes_for(:customer)
+      sign_in @member
+      post :create, format: :json, params: { customer: customer_params }
+      expect(response.content_type).to eq('application/json')
+    end
+
     it 'Flash Notice' do
       customer_params = attributes_for(:customer)
       sign_in @member
