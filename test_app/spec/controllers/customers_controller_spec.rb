713188b3943446bb4817ed22972aa055563a9e57
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-12-07 22:35:08 -0300

    Shoulda Matchers

--- test_app/spec/controllers/customers_controller_spec.rb
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -28,6 +28,10 @@ RSpec.describe CustomersController, type: :controller do
       @customer = create(:customer)
     end
 
+    it 'Route' do
+      is_expected.to route(:get, '/customers').to(action: :index)
+    end
+
     it 'Content-Type JSON' do
       customer_params = attributes_for(:customer)
       sign_in @member
@@ -48,6 +52,14 @@ RSpec.describe CustomersController, type: :controller do
       expect{
         post :create, params: { customer: customer_params }
       }.to change(Customer, :count).by(1)
+    end
+
+    it 'with invalid attributes' do
+      customer_params = attributes_for(:customer, address: nil)
+      sign_in @member
+      expect{
+        post :create, params: { customer: customer_params }
+      }.not_to change(Customer, :count)
     end
 
     it 'responds a 200 response' do
