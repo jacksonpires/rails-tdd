5b95c4b377879589bd0c6270dea8529b7f1f8e8a
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-12-16 18:38:13 -0300

    Links, Forms e Querying

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -6,4 +6,19 @@ RSpec.feature "Customers", type: :feature, js: true do
       page.save_screenshot('screenshot.png')
       expect(page).to have_current_path(customers_path)
     end
+
+    it 'Creates a Customer' do
+      member = create(:member)
+      login_as(member, :scope => :member)
+
+      visit(new_customer_path)
+
+      fill_in('Name', with: Faker::Name.name)
+      fill_in('Email', with: Faker::Internet.email)
+      fill_in('Address', with: Faker::Address.street_address)
+
+      click_button('Create Customer')
+
+      expect(page).to have_content('Customer was successfully created.')
+    end
 end
--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -30,6 +30,7 @@ RSpec.configure do |config|
 
   # Devise
   config.include Devise::Test::ControllerHelpers, :type => :controller
+  config.include Warden::Test::Helpers
 
   # Shoulda Matchers
   Shoulda::Matchers.configure do |config|
