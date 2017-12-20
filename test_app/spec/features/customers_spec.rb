9f8e19e9c55eceb55c7d46d257ab9b6f23dbd0d0
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2017-12-20 19:08:06 -0300

    Find

--- test_app/spec/features/customers_spec.rb
+++ test_app/spec/features/customers_spec.rb
@@ -13,6 +13,12 @@ RSpec.feature "Customers", type: :feature, js: true do
       expect(page).to have_content('Yes!')
     end
 
+    it 'Find' do
+      visit(customers_path)
+      click_link('Add Message')
+      expect(find('#my-div').find('h1')).to have_content('Yes!')
+    end
+
     it 'Creates a Customer' do
       member = create(:member)
       login_as(member, :scope => :member)
