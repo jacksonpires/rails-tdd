b1d0451781e3dbba4db9c004ea48de80f938e5b6
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2018-01-12 18:52:34 -0300

    Matcher include_json

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -2,9 +2,27 @@ require 'rails_helper'
 
 RSpec.describe "Customers", type: :request do
   describe "GET /customers" do
-    it "works! (now write some real specs)" do
-      get customers_index_path
+    it "works! 200 OK" do
+      get customers_path
       expect(response).to have_http_status(200)
+    end
+
+    it "index - JSON 200 OK" do
+      get "/customers.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json([
+        id: 1,
+        name: "Bria Lynch",
+        email: "meu_email-1@email.com",
+      ])
+    end
+
+    it "show - JSON 200 OK" do
+      get "/customers/1.json"
+      expect(response).to have_http_status(200)
+      expect(response.body).to include_json(
+        id: 1
+      )
     end
   end
 end
