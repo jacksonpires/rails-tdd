b8bf41c7d581d57b386cf8d9740eb3e389b60d91
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2018-01-12 18:30:06 -0300

    Testando APIs

--- test_app/app/controllers/customers_controller.rb
+++ test_app/app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :authenticate_member!, except: [:index]
+  before_action :authenticate_member!, except: [:index, :show]
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
 
   # GET /customers
