7bb2445c61ab22776c4ac5dad6abff29ce897f45
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2018-01-13 14:29:24 -0300

    POST e PATCH com JSON

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -7,24 +7,63 @@ RSpec.describe "Customers", type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "index - JSON 200 OK" do
+    it "index - JSON" do
       get "/customers.json"
-      expect(response).to have_http_status(200)
       expect(response.body).to include_json([
         id: /\d/,
         name: (be_kind_of String),
-        email: (be_kind_of String),
+        email: (be_kind_of String)
       ])
     end
 
-    it "show - JSON 200 OK" do
+    it "show - JSON" do
       get "/customers/1.json"
-      expect(response).to have_http_status(200)
       expect(response.body).to include_json(
         id: /\d/,
         name: (be_kind_of String),
-        email: (be_kind_of String),
+        email: (be_kind_of String)
       )
     end
+
+    it 'create - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json"}
+
+      customers_params = attributes_for(:customer)
+      post "/customers.json", params: { customer: customers_params}, headers: headers
+
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers_params[:name],
+        email: customers_params.fetch(:email)
+      )
+    end
+
+    it 'update - JSON' do
+      member = create(:member)
+      login_as(member, scope: :member)
+
+      headers = { "ACCEPT" => "application/json"}
+
+      customers = Customer.first
+      customers.name += " - ATUALIZADO"
+
+      patch "/customers/#{customers.id}.json", params: { customer: customers.attributes}, headers: headers
+
+      expect(response.body).to include_json(
+        id: /\d/,
+        name: customers.name,
+        email: customers.email
+      )
+    end
+
+
+
+
+
+
+
   end
 end
