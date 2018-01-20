67375f3cc5b1d067c7194b0f5b9c445416271270
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2018-01-20 18:06:02 -0300

    JSON Schema Matchers

--- test_app/spec/requests/customers_spec.rb
+++ test_app/spec/requests/customers_spec.rb
@@ -2,6 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "Customers", type: :request do
   describe "GET /customers" do
+
+    it 'JSON Schema' do
+      get '/customers/1.json'
+      expect(response).to match_response_schema("customer")
+    end
+
     it "works! 200 OK" do
       get customers_path
       expect(response).to have_http_status(200)
--- test_app/spec/spec_helper.rb
+++ test_app/spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # Webmock gem
 require 'webmock/rspec'
 
+# JSON Schema
+require "json_matchers/rspec"
+
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
