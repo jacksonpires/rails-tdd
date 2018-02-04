f7e61132330a1c65ccbe2e03773b4f4be6021894
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2018-02-04 14:32:37 -0300

    Projetando a App

--- tdd_app/spec/features/welcomes_spec.rb
+++ tdd_app/spec/features/welcomes_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 feature "Welcome", type: :feature do
   scenario 'Mostra a mensagem de Bem-Vindo' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Bem-Vindo')
+  end
+
+  scenario 'Verificar o link Cadastro de Clientes' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Cadastro de Clientes')
   end
 end
