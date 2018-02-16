beda092e2493c144bbf91c40a8ce230f1a7f2d2e
Jackson Pires <jackson_pires@MacBook-Pro-de-Jackson-Pires.local>
2018-02-15 21:08:43 -0300

    Happy Path vs Sad Path

--- tdd_app/spec/features/customers_spec.rb
+++ tdd_app/spec/features/customers_spec.rb
@@ -33,6 +33,12 @@ feature "Customers", type: :feature do
     expect(page).to have_content('Cliente cadastrado com sucesso!')
     expect(Customer.last.name).to eq(customer_name)
   end
+
+  scenario 'Não cadastra um cliente inválido' do
+    visit(new_customer_path)
+    click_on('Criar Cliente')
+    expect(page).to have_content('não pode ficar em branco')
+  end
 end
 
 
